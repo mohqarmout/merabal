444e1853a3d893d28371122aed771c6170ce9027
Mohammed <mhmmade@gmail.com>
2019-12-07 01:30:30 +0200

    add enter victim route & handler

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { getCase } = require('./getCase');
-const { interCase } = require('./postCase');
+const { enterVictim } = require('./postCase');
 const { mailList } = require('./mailList');
 
-router.post('/report-case', interCase);
+router.post('/enter-victim', enterVictim);
 router.get('/get-case', getCase);
 router.get('/mail-list', mailList);
 
