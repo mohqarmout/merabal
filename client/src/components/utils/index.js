a5573f15825d3c992875e0e40c7e5a0a6fbe5245
Mohammed <mhmmade@gmail.com>
2019-12-08 11:13:11 +0200

    fix build problem

--- client/src/components/utils/index.js
+++ client/src/components/utils/index.js
@@ -1,6 +1,5 @@
 import Loading from './Loading';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import UnderConstruction from './UnderConstruction';
 
-export { Loading, Footer, Navbar, UnderConstruction };
+export { Loading, Footer, Navbar};
