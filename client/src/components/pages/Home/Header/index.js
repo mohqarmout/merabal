462f7da2f6d9b5ab0329d770a2e8f6e1ef6620c1
Mohammed <mhmmade@gmail.com>
2020-09-22 21:07:56 +0300

    Refactor typos in the client UI

--- client/src/components/pages/Home/Header/index.js
+++ client/src/components/pages/Home/Header/index.js
@@ -11,7 +11,7 @@ const Header = () => {
     <header className={styles.header}>
       <h1 className={styles.title}>For safety world for you & me</h1>
       <p className={styles.description}>
-      Don’t be afraid to speak up for yourself. Keep fighting for your dream
+      Don’t be afraid to speak up for yourself, keep fighting for your dream
       </p>
     </header>
   );
